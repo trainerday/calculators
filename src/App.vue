11888c99d8bc6c04e1d506f8b7d629405492e82f
DannyCato <dg@catorcini.com>
2025-05-19 10:33:32 -0700

    Pushes from last week

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,41 @@
 import { ref } from 'vue';
 
 let height = ref(0);
+let weight = ref(0);
 
 function adjust(amount) {
 
+}
+
+function changeWeight(e) {
+  weight.value = e.target.value;
+  console.log(weight.value);
+}
+ 
+function changeHeight(e, scale) {
+
+  console.log(e.target.value)
+}
+
+function al(str) {
+  alert(str);
 }
 
 </script>
 
 <template>
   <form>
-    <p>Weight <input required type="number" id="weight" min="0" max="1000"></p>
+    <p>Weight <input required type="number" :value="weight" @change="changeWeight($event)" id="weight" min="0" max="1000"></p>
     <p>
-      Height (ft) <input required type="number" id="feet" min="0" max="1000"> <br>
-      (in) <input required type="number" id="feet" min="0" max="11"> 
+      Height (ft) <input required type="number" @change="changeHeight($event, 12)" id="feet" min="0" max="1000"> <br>
+      (in) <input required type="number" @change="changeHeight($event, 1)" id="in" min="0" max="11"> 
     </p>
 
-    <button type="submit" @click="adjust()"></button>
+    <button type="submit" @click="adjust()">++</button>
 
   </form>
+
+  <textarea v-on:keyup.s="al('Wowza');"></textarea>
 </template>
 
 <style scoped>
